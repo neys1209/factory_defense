34371da2101cb99b35f246494167cf6333ac3e42
neys1209 <azsx003801@gmail.com>
2022-09-02 15:22:24 +0900

    unit State
    유닛에 유한상태머신기능 추가

--- Assets/Scenes/scripts/Unit/Unit.cs
+++ Assets/Scenes/scripts/Unit/Unit.cs
@@ -5,11 +5,45 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
 
-    public enum UNITTYPE { AIR, GROUND }
-    public UNITTYPE type = UNITTYPE.AIR;
+    public enum UNITTYPE { Air, Ground }
+    public UNITTYPE type = UNITTYPE.Air;
 
-    enum STATE { CREATE, ATTACK, BUILD }
+    enum STATE { Create, Attack, Build, Move }
     
+    STATE state = STATE.Create;
+
+    void ChangeState(STATE s)
+    {
+        if (state == s) return;
+        state = s;
+        switch (state)
+        { 
+            case STATE.Create:
+                break;
+            case STATE.Attack:
+                break;
+            case STATE.Build:
+                break;
+            case STATE.Move:
+                break;
+        }
+    }
+
+    void StateProcess()
+    {
+        switch (state)
+        {
+            case STATE.Create:
+                break;
+            case STATE.Attack:
+                break;
+            case STATE.Build:
+                break;
+            case STATE.Move:
+
+                break;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +54,6 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
     

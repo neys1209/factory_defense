156fbb631b1cbd4ba609c6d6fe01341805e794c0
neys1209 <azsx003801@gmail.com>
2022-09-15 10:42:56 +0900

    unit

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -20,8 +20,8 @@ public class InputManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lineRanderer = dragLine.GetComponent<LineRenderer>();
         playerMovement = transform.position;
+        lineRanderer = dragLine.GetComponent<LineRenderer>();
         lineRanderer = dragLine.GetComponent<LineRenderer>();
     }
 

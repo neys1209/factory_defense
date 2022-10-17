4448ff3b1a8e85ffd9af0a78756016fe5bccaac2
neys1209 <azsx003801@gmail.com>
2022-10-17 12:20:13 +0900

    221017-2
    3종류의 자원추가
    이로써 현제 자원의 종류는, 철,모래,석탄,실리콘

--- Assets/scripts/Resource/Resource.cs
+++ Assets/scripts/Resource/Resource.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
-    public enum Type { Air,Iron, Sand, Coal, Clicon } //Air 타입은 아무런 특징이 없는 기본 아이템. 실험용으로만 사용 가능
+    public enum Type { Air,Iron, Sand, Coal, Silicon } //Air 타입은 아무런 특징이 없는 기본 아이템. 실험용으로만 사용 가능
+    [SerializeField]
     Type _type = Type.Air;
     public Type type
     {
